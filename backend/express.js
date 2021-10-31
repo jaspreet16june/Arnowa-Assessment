3169b6b384aedd8055c1fe46202a2b573451fb3e
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-10-31 12:48:33 +0530

    Router, Model added

--- backend/express.js
+++ backend/express.js
@@ -2,25 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const PORT = process.env.PORT || 3002
 const userRouter = require("./router/userRouter");
 const authRouter = require("./router/authRouter");
 
 app.use(express.json());
 app.use(express.static("public"));
 
-
-
-
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
 
 
-app.listen("3001", function () {
-    console.log(`Server is listening to the port 3001`);
-})
-
-app.use(function (req, res) {
-    let restOfPath = path.join("./public", "404.html");
-    res.status(404).sendFile
-        (path.join(__dirname, restOfPath));
+app.listen(PORT, function () {
+    console.log(`Server is listening to the port ${PORT}`);
 })
